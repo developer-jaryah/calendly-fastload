7e2287e228ca65fe4b6a9eae02ec1f8f25af674c
developer-jaryah <159042349+developer-jaryah@users.noreply.github.com>
2025-04-28 22:27:20 +0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,17 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/calendly-fastload.min.js',
+    format: 'umd',
+    name: 'CalendlyFastLoad',
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    terser(),
+  ],
+};
