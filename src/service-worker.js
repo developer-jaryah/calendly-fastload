f0c134a5cf7d75c8005849210faa05720688bb84
developer-jaryah <159042349+developer-jaryah@users.noreply.github.com>
2025-04-28 22:08:18 +0500

    Create service-worker.js

---
+++ src/service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'calendly-fastload-v1';
+const urlsToCache = [
+  'https://assets.calendly.com/assets/external/widget.js',
+  'https://assets.calendly.com/assets/external/widget.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
